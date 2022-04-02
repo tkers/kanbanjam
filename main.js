71ba7d8eaecb7153bbe74412849bc3bedb39129a
Tijn Kersjes <tijn@divbyzero.nl>
2022-04-02 17:39:52 +0200

    Add extra column for backlog

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ window.addEventListener('load', () => {
   })
 
   setInterval(() => {
-    if (Math.random() < 0.75) return
+    if (Math.random() < 0.9) return
 
-    const todoCol = allGrids[0]
+    const backCol = allGrids[0]
     const item = document.createElement('div')
     item.className = 'item'
     item.dataset.creationDate = Date.now()
@@ -26,17 +26,24 @@ window.addEventListener('load', () => {
     itemContent.textContent = `Hello world! ${Date.now()}`
     item.appendChild(itemContent)
 
-    const newItem = todoCol.add(item, { index: -1, active: false })
-    todoCol.show([todoCol.getItem(-1)])
-  }, 1000)
+    const newItem = backCol.add(item, { index: -1, active: false })
+    backCol.show([backCol.getItem(-1)])
+  }, 350)
 
   setInterval(() => {
-    if (Math.random() < 0.8) return
+    if (Math.random() < 0.4) return
 
-    const workCol = allGrids[1]
-    const doneCol = allGrids[2]
+    const workCol = allGrids[2]
+    const doneCol = allGrids[3]
 
     const ix = workCol.getItem(0) === floatingItem ? 1 : 0
     workCol.send(ix, doneCol, 0)
-  }, 500)
+
+    setTimeout(() => {
+      const todoCol = allGrids[1]
+
+      const ixnext = todoCol.getItem(0) === floatingItem ? 1 : 0
+      todoCol.send(ixnext, workCol, 0)
+    }, 500)
+  }, 2000)
 })
