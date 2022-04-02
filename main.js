e12ea678735363a1dda5038887a0c6e7e28c95c4
Tijn Kersjes <tijn@divbyzero.nl>
2022-04-02 18:13:36 +0200

    Cleanup code

--- main.js
+++ main.js
@@ -14,36 +14,45 @@ window.addEventListener('load', () => {
     })
   })
 
-  setInterval(() => {
-    if (Math.random() < 0.9) return
-
+  const createTask = () => {
     const backCol = allGrids[0]
     const item = document.createElement('div')
     item.className = 'item'
     item.dataset.creationDate = Date.now()
     const itemContent = document.createElement('div')
     itemContent.className = 'item-content'
-    itemContent.textContent = `Hello world! ${Date.now()}`
+    itemContent.textContent = `Hello world! This is a task created on ${Date.now()}`
     item.appendChild(itemContent)
 
     const newItem = backCol.add(item, { index: -1, active: false })
     backCol.show([backCol.getItem(-1)])
+  }
+
+  const startTask = () => {
+    const todoCol = allGrids[1]
+    const workCol = allGrids[2]
+    const ix = todoCol.getItem(0) === floatingItem ? 1 : 0
+    todoCol.send(ix, workCol, 0)
+  }
+
+  const completeTask = () => {
+    const workCol = allGrids[2]
+    const doneCol = allGrids[3]
+    const ix = workCol.getItem(0) === floatingItem ? 1 : 0
+    workCol.send(ix, doneCol, 0)
+  }
+
+  setInterval(() => {
+    if (Math.random() < 0.9) return
+    createTask()
   }, 350)
 
   setInterval(() => {
     if (Math.random() < 0.4) return
-
-    const workCol = allGrids[2]
-    const doneCol = allGrids[3]
-
-    const ix = workCol.getItem(0) === floatingItem ? 1 : 0
-    workCol.send(ix, doneCol, 0)
+    completeTask()
 
     setTimeout(() => {
-      const todoCol = allGrids[1]
-
-      const ixnext = todoCol.getItem(0) === floatingItem ? 1 : 0
-      todoCol.send(ixnext, workCol, 0)
+      startTask()
     }, 500)
   }, 2000)
 })
