eb0409557cc1aeaff96a078a8d6fec5ee1e3dbeb
Tijn Kersjes <tijn@divbyzero.nl>
2022-04-02 16:41:19 +0200

    Automatically move cards

--- main.js
+++ main.js
@@ -5,4 +5,18 @@ window.addEventListener('load', () => {
     const grid = new Muuri(col, { dragEnabled: true, dragSort: () => allGrids })
     allGrids.push(grid)
   })
+
+  document.querySelector('#btn-plan').addEventListener('click', () => {
+    const todoCol = allGrids[0]
+    const workCol = allGrids[1]
+
+    todoCol.send(0, workCol, -1)
+  })
+
+  document.querySelector('#btn-work').addEventListener('click', () => {
+    const workCol = allGrids[1]
+    const doneCol = allGrids[2]
+
+    workCol.send(0, doneCol, 0)
+  })
 })
